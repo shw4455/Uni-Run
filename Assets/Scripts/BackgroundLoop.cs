db91e652b9804f9311ae2ff17e2a168e785e8572
shw4455 <shw445567@naver.com>
2021-12-02 19:10:07 +0900

    배경 스크롤링 구현
    ScrollingOjbect.cs
    BackgroundLoop.cs 작성 및
    Background 객체에 적용

--- Assets/Scripts/BackgroundLoop.cs
+++ Assets/Scripts/BackgroundLoop.cs
@@ -6,14 +6,23 @@ public class BackgroundLoop : MonoBehaviour {
 
     private void Awake() {
         // 가로 길이를 측정하는 처리
+        BoxCollider2D backgroundColider = GetComponent<BoxCollider2D>();
+        width = backgroundColider.size.x; // 배경 콜라이더의 폭을 구해서 대입
     }
 
     private void Update() {
         // 현재 위치가 원점에서 왼쪽으로 width 이상 이동했을때 위치를 리셋
+        if (transform.position.x <= -width)
+        {
+            Reposition();
+        }
     }
 
     // 위치를 리셋하는 메서드
     private void Reposition() {
-        
+        // 현재 위치에서 오른쪽 가로 길이 * 2만큼 이동
+        Vector2 offset = new Vector2(width * 2f, 0);
+        transform.position = (Vector2) transform.position + offset;
+        // offset은 Vector2 타입이지만 transform.position은 Vector3 타입이라서 그냥 더할 수가 없기에 형변환을 해주었다
     }
 }
--- Assets/Scripts/ScrollingObject.cs
+++ Assets/Scripts/ScrollingObject.cs
@@ -2,9 +2,10 @@
 
 // 게임 오브젝트를 계속 왼쪽으로 움직이는 스크립트
 public class ScrollingObject : MonoBehaviour {
-    public float speed = 10f; // 이동 속도
+    public float speed = 10f; // 초당 이동 속도
 
     private void Update() {
         // 게임 오브젝트를 왼쪽으로 일정 속도로 평행 이동하는 처리
+        transform.Translate(Vector3.left * speed * Time.deltaTime); // ( -1, 0, 0)으로 speed 만큼 이동, Time.deltaTIme은 컴퓨터의 성능차이를 해결하기 위한 역수 곱해주기
     }
 }
