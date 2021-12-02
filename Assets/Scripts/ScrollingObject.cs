ebf71875d03d3ca9c4c59a5430139ec41a632704
shw4455 <shw445567@naver.com>
2021-12-02 21:49:00 +0900

    GameManager 스크립트 및 연관 스크립트 수정
    GameManager 작성 및 연관 스크립트
    PlayerController 수정
    ScrollingOjbects 수정
    
    좀 더 세세하게 나눠서 커밋을 할 필요성을 느낌

--- Assets/Scripts/ScrollingObject.cs
+++ Assets/Scripts/ScrollingObject.cs
@@ -5,7 +5,14 @@ public class ScrollingObject : MonoBehaviour {
     public float speed = 10f; // 초당 이동 속도
 
     private void Update() {
-        // 게임 오브젝트를 왼쪽으로 일정 속도로 평행 이동하는 처리
-        transform.Translate(Vector3.left * speed * Time.deltaTime); // ( -1, 0, 0)으로 speed 만큼 이동, Time.deltaTIme은 컴퓨터의 성능차이를 해결하기 위한 역수 곱해주기
+        // 게임오버가 아니라면
+        if (!GameManager.instance.isGameover)
+        {
+            // 초당 speed의 속도로 왼쪽으로 이동
+            // 게임 오브젝트를 왼쪽으로 일정 속도로 평행 이동하는 처리
+            transform.Translate(Vector3.left * speed * Time.deltaTime); // ( -1, 0, 0)으로 speed 만큼 이동, Time.deltaTIme은 컴퓨터의 성능차이를 해결하기 위한 역수 곱해주기
+        }
+        
     }
 }
+// 근데 이러면 if문을 계속 돌려줘야하지않은가 > 아니다 다른 방법을 써도 if문을 쓰긴 해야한다
